95eee95ca5026b5ca35f4afeedec37031f55adb6
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-04 20:38:50 -0600

    add authentication page component with layout and styling

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+
+import AuthButtons from "@/components/ui/auth/AuthButtons"
+
+function page() {
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex-1 flex overflow-hidden relative justify-center items-center dark:bg-gray-800 bg-gray-100">
+        <Image
+         src="vercel.svg" alt="logo" width={200} height={200} />
+        <div className="flex flex-col gap-2 px-4 xl:ml-40 text-center md:text-start font-semibold">
+          <h1 className="text-4xl">Chat App</h1>
+          <p className="text-lg">Using nextjs - Redis</p>
+          <AuthButtons/>
+        </div>
+      </div>
+      <div className="flex-1 relative overflow-hidden justify-center items-center hidden md:flex">
+        <Image src="next.svg" alt="logo" width={200} height={200} />
+      </div>
+    </div>
+  )
+}
+
+export default page
