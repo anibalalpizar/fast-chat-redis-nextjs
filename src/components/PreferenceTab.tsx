5f203cc40aeda432e247a3868f9c2a7011f2530e
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-05 23:13:56 -0600

    add theme toggle functionality to PreferenceTab component

--- src/components/PreferenceTab.tsx
+++ src/components/PreferenceTab.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react"
 
 import { Button } from "./ui/button"
+import { useTheme } from "next-themes"
 
 function PreferenceTab() {
+  const { setTheme } = useTheme()
   return (
     <div className="flex flex-wrap gap-2 px-1 md:px-2">
-      <Button variant="outline" size="icon">
+      <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
         <SunIcon className="size-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant="outline" size="icon">
+      <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
         <MoonIcon className="size-[1.2rem] text-muted-foreground" />
       </Button>
       <Button variant="outline" size="icon">
