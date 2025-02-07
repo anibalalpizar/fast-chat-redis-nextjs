060c87f186bf197982d68efcd7fc64da9b0f58a9
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-06 23:17:26 -0600

    add sound toggle functionality to PreferenceTab component

--- src/components/PreferenceTab.tsx
+++ src/components/PreferenceTab.tsx
@@ -1,12 +1,14 @@
-'use client'
+"use client"
 
+import { useTheme } from "next-themes"
 import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react"
 
+import { usePreferences } from "@/store/usePreferences"
 import { Button } from "./ui/button"
-import { useTheme } from "next-themes"
 
 function PreferenceTab() {
   const { setTheme } = useTheme()
+  const { soundEnabled, setSoundEnabled } = usePreferences()
   return (
     <div className="flex flex-wrap gap-2 px-1 md:px-2">
       <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
@@ -15,8 +17,16 @@ function PreferenceTab() {
       <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
         <MoonIcon className="size-[1.2rem] text-muted-foreground" />
       </Button>
-      <Button variant="outline" size="icon">
-        <VolumeX className="size-[1.2rem] text-muted-foreground" />
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => setSoundEnabled(!soundEnabled)}
+      >
+        {soundEnabled ? (
+          <Volume2 className="size-[1.2rem] text-muted-foreground" />
+        ) : (
+          <VolumeX className="size-[1.2rem] text-muted-foreground" />
+        )}
       </Button>
     </div>
   )
