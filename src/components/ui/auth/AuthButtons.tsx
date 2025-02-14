468fe75bd919efb6e494fda7fc009499f770e992
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-13 23:05:36 -0600

    update AuthButtons to manage login state and disable buttons accordingly

--- src/components/ui/auth/AuthButtons.tsx
+++ src/components/ui/auth/AuthButtons.tsx
@@ -1,19 +1,25 @@
+"use client"
+
+import { useState } from "react"
 import {
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components"
+
 import { Button } from "../button"
 
 function AuthButtons() {
+  const [isLogin, setIsLogin] = useState(false)
+
   return (
     <div className="flex gap-3 flex-1 md:flex-row flex-col relative z-50">
-      <RegisterLink className="flex-1">
-        <Button className="w-full" variant="outline">
+      <RegisterLink className="flex-1" onClick={() => setIsLogin(true)}>
+        <Button className="w-full" variant="outline" disabled={!isLogin}>
           Sign up
         </Button>
       </RegisterLink>
-      <LoginLink className="flex-1">
-        <Button className="w-full" variant="default">
+      <LoginLink className="flex-1" onClick={() => setIsLogin(true)}>
+        <Button className="w-full" variant="default" disabled={isLogin}>
           Login
         </Button>
       </LoginLink>
