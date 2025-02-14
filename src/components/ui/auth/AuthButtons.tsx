78d9173443115eb9f9bbf029dcd35a62f4031c70
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-13 23:03:34 -0600

    refactor AuthButtons to use Kinde authentication links for signup and login

--- src/components/ui/auth/AuthButtons.tsx
+++ src/components/ui/auth/AuthButtons.tsx
@@ -1,14 +1,22 @@
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components"
 import { Button } from "../button"
 
 function AuthButtons() {
   return (
     <div className="flex gap-3 flex-1 md:flex-row flex-col relative z-50">
-      <Button className="w-full" variant="outline">
-        Sign up
-      </Button>
-      <Button className="w-full" variant="default">
-        Login
-      </Button>
+      <RegisterLink className="flex-1">
+        <Button className="w-full" variant="outline">
+          Sign up
+        </Button>
+      </RegisterLink>
+      <LoginLink className="flex-1">
+        <Button className="w-full" variant="default">
+          Login
+        </Button>
+      </LoginLink>
     </div>
   )
 }
