d423c9c813206b3180c322f13af21892f0b7e3c8
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-14 23:20:47 -0600

    enhance loading experience on auth callback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Loader } from "lucide-react"
+import React from "react"
 
 function page() {
   return (
-    <div>page</div>
+    <div className="mt-20 w-full flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader className="w-10 h-10 animate-spin text-muted-foreground" />
+        <h3 className="text-lg font-bold">Loading...</h3>
+        <p>Redirecting you back to the app</p>
+      </div>
+    </div>
   )
 }
 
