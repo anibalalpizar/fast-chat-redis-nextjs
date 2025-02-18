8dcb782677e142c5b2df54d809a3985c4af499fb
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-17 23:12:55 -0600

    implement checkAuthStatus function to verify user authentication and store user data in Redis

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,3 +1,28 @@
 "use server"
 
-export async function checkAuthStatus(){}
+import { redis } from "@/lib/db"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+export async function checkAuthStatus() {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user) return { success: false }
+
+  const userId = `user:${user.id}`
+  const userExists = await redis.hgetall(userId)
+
+  if (!userExists || Object.keys(userExists).length === 0) {
+    const isImageNull = user.picture?.includes("gravatar")
+    const image = isImageNull ? "" : user.picture
+
+    await redis.hset(userId, {
+      id: user.id,
+      email: user.email,
+      name: `${user.given_name} ${user.family_name}`,
+      image,
+    })
+  }
+
+  return { success: true }
+}
