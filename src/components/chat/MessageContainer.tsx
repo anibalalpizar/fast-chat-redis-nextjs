fcacaaa85d17398b274727a5498574cc72a66d1b
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-21 22:39:06 -0600

    add escape key functionality to clear selected user in MessageContainer

--- src/components/chat/MessageContainer.tsx
+++ src/components/chat/MessageContainer.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react"
+import { useSelectedUser } from "@/store/useSelectedUser"
+
 import ChatBottonBar from "./ChatBottonBar"
 import MessageList from "./MessageList"
 import ChatTopBar from "./ChatTopBar"
 
 function MessageContainer() {
+  const { setSelectedUser } = useSelectedUser()
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setSelectedUser(null)
+    }
+    document.addEventListener("keydown", handleEscape)
+    return () => document.removeEventListener("keydown", handleEscape)
+  }, [setSelectedUser])
+
   return (
     <div className="flex flex-col justify-between w-full h-full">
       <ChatTopBar />
