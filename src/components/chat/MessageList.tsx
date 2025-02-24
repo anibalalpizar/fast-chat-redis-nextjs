9215f3599507ee89187cde9fd768f61df1c3739a
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-23 21:29:19 -0600

    refactor MessageList to use selected user and current user from context, implement message fetching with React Query, and enhance scrolling behavior

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -1,20 +1,46 @@
-import { AnimatePresence, motion } from "framer-motion"
-
-import { messages, USERS } from "@/db/dummy"
-import { cn } from "@/lib/utils"
-import { Avatar, AvatarImage } from "../ui/avatar"
 import Image from "next/image"
+import { useEffect, useRef } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import { useQuery } from "@tanstack/react-query"
+import { useSelectedUser } from "@/store/useSelectedUser"
+import { getMessages } from "@/actions/message.action"
+import { cn } from "@/lib/utils"
+
+import { Avatar, AvatarImage } from "../ui/avatar"
+import { Message } from "@/db/dummy"
 
 function MessageList() {
-  const selectedUser = USERS[0]
-  const currentUser = USERS[1]
+  const { selectedUser } = useSelectedUser()
+  const { user: currentUser } = useKindeBrowserClient()
+
+  const messageContainerRef = useRef<HTMLDivElement>(null)
+
+  const { data: messages } = useQuery<Message[]>({
+    queryKey: ["messages", selectedUser?.id],
+    queryFn: async () => {
+      if (!selectedUser || !currentUser) return []
+      return await getMessages(selectedUser?.id, currentUser?.id)
+    },
+    enabled: !!selectedUser && !!currentUser,
+  })
+
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight
+    }
+  }, [messages])
 
   return (
-    <div className="w-full overflow-y-auto overflow-x-hidden h-full flex flex-col">
+    <div
+      ref={messageContainerRef}
+      className="w-full overflow-y-auto overflow-x-hidden h-full flex flex-col"
+    >
       <AnimatePresence>
-        {messages.map((message) => (
+        {messages?.map((message, index) => (
           <motion.div
-            key={`message-${message.id}`}
+            key={`message-${message.id ?? index}`}
             layout
             initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
             animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
@@ -34,11 +60,11 @@ function MessageList() {
             )}
           >
             <div className="flex gap-3 items-center">
-              {message.senderId === selectedUser.id && (
+              {message.senderId === selectedUser?.id && (
                 <Avatar className="flex justify-center items-center">
                   <AvatarImage
-                    src={selectedUser.image}
-                    alt={selectedUser.name}
+                    src={selectedUser?.image}
+                    alt={selectedUser?.name}
                     className="border-2 border-white rounded-full"
                   />
                 </Avatar>
