94335e8ce537b3c43b84f4ee8ce3e9fd5d45283b
anibalalpizar <anibal.alpizar14@gmail.com>
2025-02-24 19:47:36 -0600

    refactor MessageList to display loading skeletons while fetching messages

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -6,9 +6,10 @@ import { useQuery } from "@tanstack/react-query"
 import { useSelectedUser } from "@/store/useSelectedUser"
 import { getMessages } from "@/actions/message.action"
 import { cn } from "@/lib/utils"
+import { Message } from "@/db/dummy"
 
 import { Avatar, AvatarImage } from "../ui/avatar"
-import { Message } from "@/db/dummy"
+import MessageSkeleton from "../skeletons/MessageSkeleton"
 
 function MessageList() {
   const { selectedUser } = useSelectedUser()
@@ -16,7 +17,7 @@ function MessageList() {
 
   const messageContainerRef = useRef<HTMLDivElement>(null)
 
-  const { data: messages } = useQuery<Message[]>({
+  const { data: messages, isLoading: isMessagesLoading } = useQuery<Message[]>({
     queryKey: ["messages", selectedUser?.id],
     queryFn: async () => {
       if (!selectedUser || !currentUser) return []
@@ -38,62 +39,72 @@ function MessageList() {
       className="w-full overflow-y-auto overflow-x-hidden h-full flex flex-col"
     >
       <AnimatePresence>
-        {messages?.map((message, index) => (
-          <motion.div
-            key={`message-${message.id ?? index}`}
-            layout
-            initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
-            animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
-            exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
-            transition={{
-              opacity: { duration: 0.1 },
-              layout: {
-                type: "spring",
-                bounce: 0.3,
-                duration: messages.indexOf(message) * 0.5 + 0.2,
-              },
-            }}
-            style={{ originX: 0.5, originY: 0.5 }}
-            className={cn(
-              "flex flex-col gap-2 p-4 whitespace-pre-wrap",
-              message.senderId === currentUser.id ? "items-end" : "items-start"
-            )}
-          >
-            <div className="flex gap-3 items-center">
-              {message.senderId === selectedUser?.id && (
-                <Avatar className="flex justify-center items-center">
-                  <AvatarImage
-                    src={selectedUser?.image}
-                    alt={selectedUser?.name}
-                    className="border-2 border-white rounded-full"
+        {!isMessagesLoading &&
+          messages?.map((message, index) => (
+            <motion.div
+              key={`message-${message.id ?? index}`}
+              layout
+              initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+              animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+              exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+              transition={{
+                opacity: { duration: 0.1 },
+                layout: {
+                  type: "spring",
+                  bounce: 0.3,
+                  duration: messages.indexOf(message) * 0.5 + 0.2,
+                },
+              }}
+              style={{ originX: 0.5, originY: 0.5 }}
+              className={cn(
+                "flex flex-col gap-2 p-4 whitespace-pre-wrap",
+                message.senderId === currentUser.id
+                  ? "items-end"
+                  : "items-start"
+              )}
+            >
+              <div className="flex gap-3 items-center">
+                {message.senderId === selectedUser?.id && (
+                  <Avatar className="flex justify-center items-center">
+                    <AvatarImage
+                      src={selectedUser?.image}
+                      alt={selectedUser?.name}
+                      className="border-2 border-white rounded-full"
+                    />
+                  </Avatar>
+                )}
+                {message.messageType === "text" ? (
+                  <span className="bg-accent p-3 rounded-md max-w-sm">
+                    {message.content}
+                  </span>
+                ) : (
+                  <Image
+                    src={message.content}
+                    alt="Message Image"
+                    width={200}
+                    height={200}
+                    className="border p-2 rounded h-40 md:h-52 object-cover"
                   />
-                </Avatar>
-              )}
-              {message.messageType === "text" ? (
-                <span className="bg-accent p-3 rounded-md max-w-sm">
-                  {message.content}
-                </span>
-              ) : (
-                <Image
-                  src={message.content}
-                  alt="Message Image"
-                  width={200}
-                  height={200}
-                  className="border p-2 rounded h-40 md:h-52 object-cover"
-                />
-              )}
-              {message.senderId === currentUser.id && (
-                <Avatar className="flex justify-center items-center">
-                  <AvatarImage
-                    src={currentUser.image}
-                    alt={currentUser.name}
-                    className="border-2 border-white rounded-full"
-                  />
-                </Avatar>
-              )}
-            </div>
-          </motion.div>
-        ))}
+                )}
+                {message.senderId === currentUser.id && (
+                  <Avatar className="flex justify-center items-center">
+                    <AvatarImage
+                      src={currentUser.picture}
+                      alt={currentUser.name}
+                      className="border-2 border-white rounded-full"
+                    />
+                  </Avatar>
+                )}
+              </div>
+            </motion.div>
+          ))}
+        {isMessagesLoading && (
+          <>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <MessageSkeleton key={index} />
+            ))}
+          </>
+        )}
       </AnimatePresence>
     </div>
   )
